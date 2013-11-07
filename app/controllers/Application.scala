0d88425b5c397c7b4ca0390a0087a0e0d923cf60
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2013-11-07 17:22:58 +0000

    Updates the page. Adds pitch, symbols and new images. Missing: send data to mail chimp

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
+	val loginForm = Form(
+	  tuple(
+	    "name" -> text,
+	    "email" -> text
+	  )
+	)
+
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(loginForm))
   }
+
+  def submit = TODO
+
 }
