9625d79fe04f8080f3685d0694062e48b50ebef3
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2013-11-21 15:57:20 +0000

    Adds logic to add new email to mail chimp and to send notification email to myself

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import play.api.mvc.Action
 import play.api.db.DB
 import play.api.Play.current
 import anorm._ 
+import play.api.libs.concurrent.Execution.Implicits._
 
 object Application extends Controller {
    
@@ -50,17 +51,15 @@ object DatabaseService {
   
   private def init() {
     DB.withConnection { implicit conn =>
-      val qs = "CREATE TABLE IF NOT EXISTS Contacts(user_id SERIAL NOT NULL PRIMARY KEY,email varchar(225) NOT NULL UNIQUE)"
-      SQL(qs).execute
+      val query = "CREATE TABLE IF NOT EXISTS Contacts(user_id SERIAL NOT NULL PRIMARY KEY,email varchar(225) NOT NULL UNIQUE)"
+      SQL(query).execute
     }
   }
   
   def getAllContacts() : Unit = {
     DB.withConnection { implicit conn =>
       val selectedContacts = SQL("Select * from Contacts")
-      val contacts = selectedContacts().map(row => 
-        row[String]("email")
-      ).toList
+      val contacts = selectedContacts().map(_[String]("email")).toList
 
       println(contacts.toString) 
     }
@@ -69,9 +68,7 @@ object DatabaseService {
   def exists(email: String) : Boolean = {
     init
     DB.withConnection { implicit conn =>
-      val number = SQL("Select * from Contacts Where email='" + email + "'")().map(row =>
-        row[String]("email")
-      ).toList.size
+      val number = SQL("Select * from Contacts Where email='" + email + "'")().map( _ [String]("email")).toList.size
       
       number match {
         case 1 => true
@@ -85,8 +82,25 @@ object DatabaseService {
     DB.withConnection { implicit conn =>            
       val id: Option[Long] = SQL("insert into Contacts(email) values ({email})")
               .on('email -> email).executeInsert()
-      println(id)
       getAllContacts
+      updateMailchimpContacts(email)
+      sendNotificationEmail(email)
     }
+  }
+  
+  private def updateMailchimpContacts(email: String) : Unit = {
+    val response = MailChimp.subscribe(email)
+    println("Mailchimp update notification")
+    response.get.map({
+      response => println(response.body)
+    })
+  }
+  
+  private def sendNotificationEmail(emailContact: String) : Unit = {
+    val response = SendGrid.sendEmail(emailContact)
+    println("Email notification")
+    response.map({
+      response => println(response.body)
+    })
   }
 }
