71c04bf00caaf592d6e7090ec47498bb94562893
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-06-08 01:55:00 +0100

    saving work part2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,7 +10,8 @@ $(document).ready(function($) {
 		animation: "fade",
 		slideshowSpeed: 3500,
 		controlNav: false,
-		directionNav: false
+		directionNav: false,
+		smoothHeight:true
 	});
 });
 
@@ -41,9 +42,9 @@ $(document).ready(function($) {
 });
 
 // Prettyphoto
-$(document).ready(function() {
-	$("a[class^='prettyPhoto']").prettyPhoto({theme:'pp_default'});
-});
+//($(document).ready(function() {
+//	$("a[class^='prettyPhoto']").prettyPhoto({theme:'pp_default'});
+//});
 
 
 
@@ -57,3 +58,205 @@ $(document).ready(function(){
 	$('#second').parallax("50%", 0.1);
 });
 
+
+$(document).ready(function(e) {
+	$('.with-hover-text, .regular-link').click(function(e){
+		e.stopPropagation();
+	});
+	
+	/***************
+	* = Hover text *
+	* Hover text for the last slide
+	***************/
+	$('.with-hover-text').hover(
+		function(e) {
+			$(this).css('overflow', 'visible');
+			$(this).find('.hover-text')
+				.show()
+				.css('opacity', 0)
+				.delay(200)
+				.animate(
+					{
+						paddingTop: '25px',
+						opacity: 1
+					},
+					'fast',
+					'linear'
+				);
+		},
+		function(e) {
+			var obj = $(this);
+			$(this).find('.hover-text')
+				.animate(
+					{
+						paddingTop: '0',
+						opacity: 0
+					},
+					'fast',
+					'linear',
+					function() {
+						$(this).hide();
+						$( obj ).css('overflow', 'hidden');
+					}
+				);
+		}
+	);
+	
+	var img_loaded = 0;
+	var j_images = [];
+	
+});
+
+
+var delay = (function(){
+	var timer = 0;
+	return function(callback, ms){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+
+function menu_focus( element, i ) {
+	if ( $(element).hasClass('active') ) {
+		if ( i == 6 ) {
+			if ( $('.navbar').hasClass('inv') == false )
+				return;
+		} else {
+			return;
+		}
+	}
+	
+	enable_arrows( i );
+		
+	if ( i == 1 || i == 6 )
+		$('.navbar').removeClass('inv');
+	else
+		$('.navbar').addClass('inv');
+	
+	$('.nav > li').removeClass('active');
+	$(element).addClass('active');
+	
+	var icon = $(element).find('.icon');
+	
+	var left_pos = icon.offset().left - $('.nav').offset().left;
+	var el_width = icon.width() + $(element).find('.text').width() + 10;
+	
+	$('.active-menu').stop(false, false).animate(
+		{
+			left: left_pos,
+			width: el_width
+		},
+		1500,
+		'easeInOutQuart'
+	);
+}
+
+function enable_arrows( dataslide ) {
+	$('#arrows div').addClass('disabled');
+	if ( dataslide != 1 ) {
+		$('#arrow-up').removeClass('disabled');
+	}
+	if ( dataslide != 6 ) {
+		$('#arrow-down').removeClass('disabled');
+	}
+	if ( dataslide == 3 ) {
+		$('#arrow-left').removeClass('disabled');
+		$('#arrow-right').removeClass('disabled');
+	}
+}
+
+/***************
+* = Menu hover *
+***************/
+jQuery(document).ready(function ($) {
+	//Cache some variables
+	var menu_item = $('.nav').find('li');
+	
+	menu_item.hover(
+		function(e) {
+			var icon = $(this).find('.icon');
+			
+			var left_pos = icon.offset().left - $('.nav').offset().left;
+			var el_width = icon.width() + $(this).find('.text').width() + 10;
+			
+			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
+				.css('left', left_pos)
+				.css('width', el_width)
+				.attr('id', 'special-active-menu-' + $(this).data('slide') );
+			
+			$('.active-menu').after( hover_bar );
+		},
+		function(e) {
+			$('.special-active-menu').remove();
+		}
+	);
+});
+
+$(document).ready(function(e) {
+
+  $(".fancybox").fancybox({
+      padding: 10,
+      helpers: {
+          overlay: {
+              locked: false
+          }
+      }
+  });
+
+  mixpanel.track('pageView');
+
+  var emailSubmit = function(email) {
+    var validator = function (email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    };
+
+    var emailValue = $(email).val();
+    var result = validator(emailValue);
+    if (result) {
+        $.post( "/submit", {email: emailValue}, function(data) {
+          mixpanel.track("Sign Up", {"email": emailValue});
+          $("#shareText").attr('hidden', false);
+          $("#share").attr('hidden', false);
+          alert("Thanks for joining us! We have awesome news coming soon. Stay tuned");
+        })
+        .fail(function(data) {
+            alert("There was an error with your submission. Please try again");
+        });
+    } else {
+        alert("Please insert a valid email address");
+    }
+  };
+
+  $("#saveUserEmail").on("click", function(){
+    emailSubmit("#userEmail");
+  });
+  $("#saveUserEmailUp").on("click", function(){
+    emailSubmit("#userEmailUp");
+  });
+  $('#share').share({
+    networks: ['googleplus','facebook','twitter','linkedin'],
+    theme: 'square',
+    urlToShare: "http://www.usewazza.com",
+    title: "I just signed up for Wazza! Join me!"
+  });
+});
+
+
+/*
+* smooth scrolling hack
+*/
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
