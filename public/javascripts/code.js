5880df708c3c6c92c87b1e4471f6ad4e9b3c1dd2
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-06-17 14:38:49 +0100

    revamp of' how does it work' page

---
+++ public/javascripts/code.js
@@ -0,0 +1,139 @@
+example_timeline = [
+ {"delay": 650, "blit": [[0, 0, 1366, 768, 0, 0]]},
+ {"delay": 650, "blit": [[1163, 901, 9, 8, 181, 44], [938, 914, 35, 14, 84, 149], [1094, 858, 156, 11, 101, 752]]},
+ {"delay": 650, "blit": [[1193, 913, 36, 14, 118, 149], [39, 753, 5, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1229, 913, 8, 14, 153, 149], [1193, 834, 54, 12, 204, 752], [32, 753, 5, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1040, 919, 15, 14, 160, 149], [1243, 911, 54, 12, 204, 752], [1308, 849, 5, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1092, 915, 28, 14, 174, 149], [1304, 899, 13, 9, 101, 753]]},
+ {"delay": 650, "blit": [[995, 905, 49, 14, 203, 149], [938, 859, 6, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1029, 890, 70, 15, 196, 149], [134, 965, 13, 9, 182, 151], [1253, 796, 63, 12, 84, 178], [1243, 899, 61, 12, 196, 752], [1321, 849, 12, 9, 101, 753]]},
+ {"delay": 650, "blit": [[1150, 913, 43, 15, 258, 149], [1035, 834, 61, 12, 196, 752], [39, 753, 5, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1316, 783, 15, 15, 293, 149], [1087, 846, 149, 12, 108, 752]]},
+ {"delay": 650, "blit": [[1019, 919, 21, 14, 301, 150], [945, 846, 131, 12, 108, 752]]},
+ {"delay": 650, "blit": [[1199, 884, 70, 15, 308, 149], [938, 834, 158, 12, 100, 752]]},
+ {"delay": 650, "blit": [[1297, 911, 36, 15, 363, 149], [1049, 880, 150, 10, 108, 752]]},
+ {"delay": 650, "blit": [[1298, 862, 35, 15, 392, 149], [1243, 899, 61, 12, 196, 752], [1316, 798, 14, 9, 100, 753]]},
+ {"delay": 650, "blit": [[1269, 884, 64, 15, 412, 149], [1035, 834, 61, 12, 196, 752], [938, 859, 6, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1301, 834, 29, 15, 461, 149], [1150, 901, 13, 9, 101, 753]]},
+ {"delay": 650, "blit": [[1199, 869, 99, 15, 482, 149], [1317, 899, 13, 9, 101, 753]]},
+ {"delay": 650, "blit": [[1309, 768, 22, 15, 566, 149], [1309, 790, 7, 1, 252, 163], [84, 178, 63, 12, 84, 178], [944, 847, 6, 9, 107, 753]]},
+ {"delay": 650, "blit": [[1099, 901, 51, 14, 0, 149], [1332, 768, 1, 14, 587, 149], [1353, 150, 10, 7, 1353, 149], [1309, 789, 7, 1, 420, 205], [1187, 899, 56, 14, 0, 289], [0, 2110, 790, 473, 84, 290], [945, 847, 5, 9, 32, 753]]},
+ {"delay": 650, "blit": [[1331, 768, 1, 14, 55, 289], [203, 223, 8, 7, 112, 293], [938, 859, 5, 9, 101, 753]]},
+ {"delay": 650, "blit": [[1309, 788, 7, 1, 420, 205], [938, 869, 111, 15, 134, 289], [36, 1765, 12, 7, 120, 293], [1309, 787, 7, 1, 84, 667], [938, 846, 149, 12, 101, 752]]},
+ {"delay": 650, "blit": [[866, 1449, 496, 2, 7, 20], [1309, 786, 7, 1, 420, 205], [998, 919, 21, 15, 231, 289], [1309, 785, 7, 1, 84, 667], [1035, 834, 61, 12, 196, 752], [938, 859, 5, 9, 108, 753]]},
+ {"delay": 650, "blit": [[7, 20, 496, 2, 7, 20], [1353, 211, 10, 7, 1353, 210], [1250, 834, 51, 28, 0, 289], [0, 2583, 790, 459, 84, 289], [112, 276, 118, 27, 112, 304], [28, 2138, 111, 13, 112, 332], [938, 858, 156, 11, 101, 752], [32, 753, 5, 9, 39, 753]]},
+ {"delay": 650, "blit": [[938, 768, 315, 40, 84, 64], [1333, 768, 21, 235, 15, 65], [938, 821, 395, 13, 407, 92], [0, 1467, 837, 643, 84, 105], [1353, 63, 10, 67, 1353, 149], [866, 768, 51, 445, 0, 303], [1353, 150, 10, 67, 1353, 552], [1099, 890, 88, 11, 32, 752]]},
+ {"delay": 650, "blit": [[188, 205, 7, 14, 139, 399], [1096, 834, 151, 12, 114, 752]]},
+ {"delay": 650, "blit": [[954, 870, 38, 10, 150, 400], [1243, 911, 54, 12, 211, 752], [1187, 890, 12, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1044, 905, 48, 14, 190, 399], [945, 847, 5, 9, 115, 753]]},
+ {"delay": 650, "blit": [[111, 1495, 19, 14, 237, 399], [1321, 849, 12, 9, 108, 753]]},
+ {"delay": 650, "blit": [[0, 768, 866, 699, 55, 64], [917, 768, 21, 681, 15, 65], [938, 808, 395, 13, 407, 92], [506, 838, 300, 13, 561, 134], [528, 388, 24, 11, 864, 134], [1354, 768, 10, 183, 1353, 436], [1301, 849, 20, 9, 32, 753]]},
+ {"delay": 650, "blit": [[1253, 768, 56, 28, 0, 385], [1201, 917, 13, 7, 140, 389], [1049, 869, 149, 11, 115, 752], [1094, 859, 6, 9, 46, 753]]},
+ {"delay": 650, "blit": [[215, 752, 142, 11, 122, 752]]},
+ {"delay": 650, "blit": [[1120, 915, 27, 14, 155, 385], [938, 859, 5, 9, 115, 753]]},
+ {"delay": 650, "blit": [[973, 919, 25, 14, 178, 385], [1304, 899, 13, 9, 108, 753]]},
+ {"delay": 650, "blit": [[1309, 784, 7, 1, 609, 161], [938, 899, 57, 15, 202, 385], [938, 859, 6, 9, 115, 753]]},
+ {"delay": 600, "blit": [[1309, 783, 7, 1, 609, 161], [938, 884, 91, 15, 238, 385], [1236, 846, 13, 9, 107, 753]]}];
+
+var delay_scale = 0.7
+var timer = null
+
+var animate = function(img, timeline, element)
+{
+	var i = 0
+
+	var run_time = 0
+	for (var j = 0; j < timeline.length - 1; ++j)
+		run_time += timeline[j].delay
+
+	var f = function()
+	{
+		var frame = i++ % timeline.length
+		var delay = timeline[frame].delay * delay_scale
+		var blits = timeline[frame].blit
+
+		var ctx = element.getContext('2d')
+
+		for (j = 0; j < blits.length; ++j)
+		{
+			var blit = blits[j]
+			var sx = blit[0]
+			var sy = blit[1]
+			var w = blit[2]
+			var h = blit[3]
+			var dx = blit[4]
+			var dy = blit[5]
+			ctx.drawImage(img, sx, sy, w, h, dx, dy, w, h)
+		}
+
+		timer = window.setTimeout(f, delay)
+	}
+
+	if (timer) window.clearTimeout(timer)
+	f()
+}
+
+var animate_fallback = function(img, timeline, element)
+{
+	var i = 0
+
+	var run_time = 0
+	for (var j = 0; j < timeline.length - 1; ++j)
+		run_time += timeline[j].delay
+
+	var f = function()
+	{
+		if (i % timeline.length == 0)
+		{
+			while (element.hasChildNodes())
+				element.removeChild(element.lastChild)
+		}
+
+		var frame = i++ % timeline.length
+		var delay = timeline[frame].delay * delay_scale
+		var blits = timeline[frame].blit
+
+		for (j = 0; j < blits.length; ++j)
+		{
+			var blit = blits[j]
+			var sx = blit[0]
+			var sy = blit[1]
+			var w = blit[2]
+			var h = blit[3]
+			var dx = blit[4]
+			var dy = blit[5]
+
+			var d = document.createElement('div')
+			d.style.position = 'absolute'
+			d.style.left = dx + "px"
+			d.style.top = dy + "px"
+			d.style.width = w + "px"
+			d.style.height = h + "px"
+			d.style.backgroundImage = "url('" + img.src + "')"
+			d.style.backgroundPosition = "-" + sx + "px -" + sy + "px"
+
+			element.appendChild(d)
+		}
+
+		timer = window.setTimeout(f, delay)
+	}
+
+	if (timer) window.clearTimeout(timer)
+	f()
+}
+
+function set_animation(img_url, timeline, canvas_id, fallback_id)
+{
+	var img = new Image()
+	img.onload = function()
+	{
+		var canvas = document.getElementById(canvas_id)
+		if (canvas && canvas.getContext)
+			animate(img, timeline, canvas)
+		else
+			animate_fallback(img, timeline, document.getElementById(fallback_id))
+	}
+	img.src = img_url
+}
+
+set_animation("../assets/images/code_packed.png", example_timeline, 'anim_target', 'anim_fallback');
