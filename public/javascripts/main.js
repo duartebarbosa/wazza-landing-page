7c7cdfcff5896448a97b7aa910f605d9f0d5a2b0
Duarte Barbosa <duarte.barbosa@gmail.com>
2014-06-18 16:06:58 +0100

    new icons, bling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -151,46 +151,6 @@ function menu_focus( element, i ) {
 	);
 }
 
-function enable_arrows( dataslide ) {
-	$('#arrows div').addClass('disabled');
-	if ( dataslide != 1 ) {
-		$('#arrow-up').removeClass('disabled');
-	}
-	if ( dataslide != 6 ) {
-		$('#arrow-down').removeClass('disabled');
-	}
-	if ( dataslide == 3 ) {
-		$('#arrow-left').removeClass('disabled');
-		$('#arrow-right').removeClass('disabled');
-	}
-}
-
-/***************
-* = Menu hover *
-***************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var menu_item = $('.nav').find('li');
-	
-	menu_item.hover(
-		function(e) {
-			var icon = $(this).find('.icon');
-			
-			var left_pos = icon.offset().left - $('.nav').offset().left;
-			var el_width = icon.width() + $(this).find('.text').width() + 10;
-			
-			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
-				.css('left', left_pos)
-				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
-			$('.active-menu').after( hover_bar );
-		},
-		function(e) {
-			$('.special-active-menu').remove();
-		}
-	);
-});
 
 $(document).ready(function(e) {
 
