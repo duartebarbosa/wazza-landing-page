fa6b4bf86bb9ef22817e0d155687569de9d28062
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-02-18 15:16:52 +0000

    adds mouseover on promocode for explanation; on success -> sends data to mixpanel

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,31 @@
 
 $(document).ready(function() {
 
-	//mixpanel.track('pageView');
+  var originalLeave = $.fn.popover.Constructor.prototype.leave;
+  $.fn.popover.Constructor.prototype.leave = function(obj){
+    var self = obj instanceof this.constructor ?
+          obj : $(obj.currentTarget)[this.type](this.getDelegateOptions()).data('bs.' + this.type)
+      var container, timeout;
+      
+      originalLeave.call(this, obj);
+
+    if(obj.currentTarget) {
+      container = $(obj.currentTarget).siblings('.popover')
+      timeout = self.timeout;
+      container.one('mouseenter', function(){
+          //We entered the actual popover – call off the dogs
+        clearTimeout(timeout);
+        //Let's monitor popover content instead
+        container.one('mouseleave', function(){
+          $.fn.popover.Constructor.prototype.leave.call(self, self);
+        });
+      })
+    }
+  };
+
+  $('body').popover({ selector: '[data-popover]', trigger: 'click hover', placement: 'auto', delay: {show: 50, hide: 400}});
+    
+	mixpanel.track('pageView');
 
   $("#requestContactForm").submit(function(e) {
     //prevent Default functionality
@@ -22,29 +46,32 @@ $(document).ready(function() {
     });
 
     var actionUrl = e.currentTarget.action;
-    console.log(data);
-    console.log(actionUrl);
-
+    data["country"] = $("#countrySelector").val();
+    
     $.post(actionUrl, data)
       .done(function(r) {
-        console.log("success");
+        mixpanel.register({
+          "Name": data.name,
+          "Email": data.email,
+          "Company": data.company,
+          "Country": data.country
+        });
+        mixpanel.people.set({
+          "name": data.name,
+          "$email": data.email,
+          "$create": new Date(),
+          "company": data.company
+        });
+        mixpanel.identify(data.email);
+        mixpanel.track("Sign Up");
+        $("#onSuccess").attr('hidden', false);
+        $("#onFailure").attr('hidden', true);
       })
       .fail(function(e){
-        console.log("error");
+        $("#onSuccess").attr('hidden', true);
+        $("#onFailure").attr('hidden', false);
       })
   });
-
-	// $("#saveUserEmail").on("click", function(){
-  //   console.log("email submit");
-    
-  //   $.post( "/submit", {email: $("#userEmail").val()}, function(data) {
-	// 		mixpanel.track("Sign Up", {"email": data.emailValue});
-	// 		//$("#shareText").attr('hidden', false);
-  //     console.log("SUCCESS");
-	// 		$("#share").attr('hidden', false);
-	// 		$("#asksales").addClass('hidden');
-	// 	});
-	// });
 
 	/*
 	* smooth scrolling hack
